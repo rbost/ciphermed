ce21105bba0753d3f7e2580f1bd7739ee5ab7870
Raphael Bost <raphael@csail.mit.edu>
2013-10-29 00:58:25 -0400

    Improve the makefile

--- src/protobuf/protobuf_conversion.hh
+++ src/protobuf/protobuf_conversion.hh
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <gmpxx.h>
-#include "bigint.pb.h"
+#include <proto_src/bigint.pb.h>
 
 mpz_class bigint_message_to_mpz_class(const Protobuf::BigInt &m);
 Protobuf::BigInt mpz_class_to_bigint_message(const mpz_class &v);
