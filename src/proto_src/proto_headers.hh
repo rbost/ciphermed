7c38802398a8da1e9100d126fa7c4d4e6ca7951a
Raphael Bost <raphael@csail.mit.edu>
2013-11-04 16:48:31 -0500

    Mistake in the gitignore file that lead to a compilation error.

---
+++ src/proto_src/proto_headers.hh
@@ -0,0 +1,7 @@
+//#include <proto_src/bigint.pb.h>
+//#include <proto_src/keys.pb.h>
+//#include <proto_src/lsic_messages.pb.h>
+
+#include "bigint.pb.h"
+#include "keys.pb.h"
+#include "lsic_messages.pb.h"
