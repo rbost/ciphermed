ee88031a8b700506441b40c8a3752408710e4de2
Raphael Bost <raphoufoune@gmail.com>
2015-06-09 20:13:18 +0200

    Remove useless files

--- src/crypto/test_crypto.cc
+++ src/crypto/test_crypto.cc
@@ -20,7 +20,6 @@
 
 #include <assert.h>
 #include <vector>
-#include <crypto/elgamal.hh>
 #include <crypto/paillier.hh>
 #include <crypto/gm.hh>
 #include <NTL/ZZ.h>
@@ -33,33 +32,6 @@
 
 using namespace std;
 using namespace NTL;
-
-
-static void
-test_elgamal()
-{
-    cout << "Test ElGamal ..." << flush;
-    auto sk = ElGamal_priv::keygen();
-    ElGamal_priv pp(sk);
-    
-    auto pk = pp.pubkey();
-    ElGamal p(pk);
-    
-    ZZ pt0 = RandomLen_ZZ(256);
-    ZZ pt1 = RandomLen_ZZ(256);    
-
-    pair<ZZ,ZZ> ct0 = p.encrypt(pt0);
-    pair<ZZ,ZZ> ct1 = p.encrypt(pt1);
-    pair<ZZ,ZZ> prod = p.mult(ct0, ct1);
-	pair<ZZ,ZZ> scal = p.scalarize(p.encrypt(to_ZZ(1)));
-
-    assert(pp.decrypt(ct0) == pt0);
-    assert(pp.decrypt(ct1) == pt1);
-    assert(pp.decrypt(prod) == (pt0 * pt1));
-    assert(pp.decrypt(scal) == to_ZZ(1));
-    
-    cout << " passed" << endl;
-}
 
 static void
 test_paillier()
--- src/math/field.cc
+++
@@ -1,9 +0,0 @@
-#include "math/field.hh"
-
-#include <vector>
-#include <sstream>
-#include <iostream>
-
-#include <util/util.hh>
-
-using namespace std;
